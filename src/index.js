7b93f53c463e43fad174708afd71c47acfe393e1
Erin Yant <ghosrider383@gmail.com>
2024-05-09 08:32:04 -0600

    completed with no errors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Directors from "./pages/Directors";
+import Actors from "./pages/Actors";
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <Home />
+    },
+    {
+        path: "/movie",
+        element: <Movie />
+    },
+    {
+        path:"/directors",
+        element: <Directors />
+    },
+    {
+        path:"/actors",
+        element: <Actors />
+    },
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<RouterProvider router={router} />);
